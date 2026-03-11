42a947f5207187a8c0294eebd0b34320bd8e4a2b
SS0032 <sahilworks3637@gmail.com>
2026-03-11 20:28:33 +0530

    HTTP Exception(404)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, HTTPException
 import json
 
 app = FastAPI()
@@ -27,4 +27,4 @@ def view_patient(patient_id: str=Path(..., description="The ID of the patient to
     if patient_id in data:
         return data[patient_id]
     else:
-        return {"error":"Patient not found"}
+        raise HTTPException(status_code=404, detail="Patient not found")
